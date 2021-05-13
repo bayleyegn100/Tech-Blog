948f258f8e2632553b1eaa13094d20d3337b6a83
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-13 05:23:38 -0700

    Foreign keys and respective alias defined

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const BlogPost = require('./BlogPost');
+const BlogComment = require('./BlogComment');
+// Foreign keys and respective alias defined
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogPost.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogComment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+BlogComment.belongsTo(BlogPost, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(BlogComment, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, BlogPost, BlogComment };
