b19054c84f9aa600ea829287459fff86ff2f349a
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-13 06:20:48 -0700

    Middleware mounted

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-
+//  To mount middleware for the routes, router.use used.
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
