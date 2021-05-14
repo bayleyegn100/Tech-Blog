12c046300112f405428b2ba2f0f0d052d1848112
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-14 06:27:13 -0700

    seed database defined

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, BlogPost } = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./blogPostData.json');
+const blogPostData = require('./blogPostData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
