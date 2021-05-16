bda1a869458520a8e84f7e9fd43327956371f046
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-16 16:48:06 -0700

    helper imported

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const routes = require('./controllers');
+const path = require('path');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -15,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Keep for your self!',
+  secret: 'Keep it for your self!',
   cookie: {},
   resave: false,
   saveUninitialized: true,
