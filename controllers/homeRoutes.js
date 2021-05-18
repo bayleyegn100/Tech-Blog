80a3fc5f1ee56f22cc48cf77f189b016584d21ef
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-17 22:20:04 -0700

    Putting attribute value in side "".

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,6 +20,7 @@ router.get("/", async (req, res) => {
     // Pass serialized data and session flag into template
     res.render("homepage", {
       blogPosts,
+  
       logged_in: req.session.logged_in,
     });
   } catch (err) {
