2057fbda18752bd8f63e36aab4142ab45a6ad74d
bayleyegn100 <bayleyegn100@gmail.com>
2021-05-26 22:44:21 -0700

    Added a delete button.

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,34 +1,51 @@
 const updateFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#post-title').value.trim();
-  const body = document.querySelector('#post-body').value.trim();
-  const id = +window.location.toString().split("/")[5]
+  const title = document.querySelector("#post-title").value.trim();
+  const body = document.querySelector("#post-body").value.trim();
+  const id = +window.location.toString().split("/")[5];
 
-
-  console.log(id)
+  console.log(id);
   if (title && body) {
     const response = await fetch(`/api/blogPosts/${id}`, {
-      method: 'PUT',
+      method: "PUT",
       body: JSON.stringify({
         title,
-        body
+        body,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
-      alert('Failed to create blog post');
+      alert("Failed to create blog post");
     }
   }
 };
-
-const updateButton = document.querySelector('#update-button')
+async function deletePost(e) {
+  e.preventDefault();
+  const id = window.location.toString().split("/")[window.location.toString().split("/").length -1];
+  const response = await fetch(`/api/blogPosts/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to delet blog post");
+  }
+}
+const updateButton = document.querySelector("#update-button");
 
 if (updateButton !== null) {
-  updateButton.addEventListener('click', updateFormHandler);
+  updateButton.addEventListener("click", updateFormHandler);
+}
+const deleteButton = document.querySelector("#delete-button");
+if (deleteButton !== null) {
+  deleteButton.addEventListener("click", deletePost);
 }
